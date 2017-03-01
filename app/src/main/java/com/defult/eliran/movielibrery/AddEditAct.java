8e6eeb176b0f644a55c877ed7a4a8483f4915875
eliranmoshe <kaza.moshe@gmail.com>
2017-03-01 13:20:28 +0200

    with downloadimage from camera  and no download image from manualy

--- app/src/main/java/com/defult/eliran/movielibrery/AddEditAct.java
+++ app/src/main/java/com/defult/eliran/movielibrery/AddEditAct.java
@@ -46,7 +46,6 @@ import java.net.URL;
 import java.net.URLConnection;
 
 
-
 public class AddEditAct extends AppCompatActivity implements View.OnClickListener {
     //Edit exist movie | Add new Manual movie | Add new Internet Movie
     SqlHelper sqlHelper;
@@ -147,13 +146,16 @@ public class AddEditAct extends AppCompatActivity implements View.OnClickListene
         switch (v.getId()) {
             case R.id.ShowBtn:
                 //set image bitmap with decode
+
                 android.widget.PopupMenu popupMenu = new android.widget.PopupMenu(getApplicationContext(), v);
                 popupMenu.inflate(R.menu.cameramenu);
                 popupMenu.show();
                 popupMenu.setOnMenuItemClickListener(new android.widget.PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
+
                         switch (item.getItemId()) {
+
                             case R.id.CameraMenu:
                                 if (checkSelfPermission(Manifest.permission.READ_CONTACTS)
                                         != PackageManager.PERMISSION_GRANTED) {
@@ -161,24 +163,17 @@ public class AddEditAct extends AppCompatActivity implements View.OnClickListene
                                 }
                                 break;
                             case R.id.DownImageMenu:
-
-                                Picasso.with(AddEditAct.this).load(UrlET.getText().toString()).into(movieIV);
-                                //TODO set bitmap to imagebase64
-
+                                if (UrlET.getText().toString().contains("https://")) {
+                                    Picasso.with(AddEditAct.this).load(UrlET.getText().toString()).into(movieIV);
+                                } else {
+                                    Toast.makeText(AddEditAct.this, "no image to download", Toast.LENGTH_SHORT).show();
+                                }
                                 break;
                             //TODO chose from gallery
                         }
-
                         return true;
                     }
                 });
-                //TODO if its from edit make add show image url button or film from camera
-
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant
-
-
                 break;
             case R.id.OkBtn:
 
@@ -197,8 +192,7 @@ public class AddEditAct extends AppCompatActivity implements View.OnClickListene
                     contentValues.put(DbConstant.rating, RatingET.getText().toString());
                     imagebase64 = encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
                     contentValues.put(DbConstant.imagebase64, imagebase64);
-                }
-                else{
+                } else {
                     Toast.makeText(this, "please enter movie name", Toast.LENGTH_SHORT).show();
                 }
                 if (DbConstant.isEditAct == true) {
@@ -231,137 +225,134 @@ public class AddEditAct extends AppCompatActivity implements View.OnClickListene
                 }
 
 
-
-
-
-        break;
-        case R.id.CancelBtn:
-        //TODO how to make that if there is no changes dont alert dialogd
-        AlertDialog.Builder alert = new AlertDialog.Builder(AddEditAct.this);
-        alert.setIcon(R.drawable.download);
-        //getResources().getString(R.string.nassage);
-        alert.setMessage("ALL DETAILS WILL BE LOST\n" + "ARE YOU SURE YOU WANT TO BACK")
-                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_righ);
-                        dialog.cancel();
-                    }
-                })
-                .setNegativeButton("STAY", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-        alert.setTitle("WARNING");
-        alert.create().show();
-        break;
-    }
-
-}
-
-public class DownLoadBody extends AsyncTask<String, Long, String> {
-    //if AddEditAct start From InternetAct Auto start AsyncTask to download the Body
-    ProgressDialog progressDialog;
-
-    @Override
-    protected void onPreExecute() {
-        // popup progressdialog while loading movie body
-        progressDialog = new ProgressDialog(AddEditAct.this);
-        progressDialog.setTitle("loading data111! \n please wait.....");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-    }
-
-    @Override
-    protected String doInBackground(String... params) {
-        //get the api of the ImdbID
-        StringBuilder response = null;
-        response = new StringBuilder();
-        try {
-            URL website = new URL(params[0]);
-            URLConnection connection = website.openConnection();
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            connection.getInputStream()));
-
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                response.append(inputLine);
-            in.close();
-
-        } catch (Exception ee) {
-            ee.printStackTrace();
-        }
-        if (response == null) {
-            response.append("error");
+                break;
+            case R.id.CancelBtn:
+                //TODO how to make that if there is no changes dont alert dialogd
+                AlertDialog.Builder alert = new AlertDialog.Builder(AddEditAct.this);
+                alert.setIcon(R.drawable.download);
+                //getResources().getString(R.string.nassage);
+                alert.setMessage("ALL DETAILS WILL BE LOST\n" + "ARE YOU SURE YOU WANT TO BACK")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_righ);
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton("STAY", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                alert.setTitle("WARNING");
+                alert.create().show();
+                break;
         }
 
-        return response.toString();
-
-
     }
 
-    @Override
-    protected void onPostExecute(String resultObject) {
-        //convert Api to JSON and SetText to BodyET
-        try {
-            if (resultObject == "error") {
-                Toast.makeText(AddEditAct.this, "DID NOT FIND MOVIE,PLEASE TRY AGAIN.", Toast.LENGTH_SHORT).show();
-            } else {
-                JSONObject mainObject = new JSONObject(resultObject);
-                Log.d("************dsds", resultObject);
-                BodyET.setText(mainObject.getString("Plot"));
-                RatingET.setText(mainObject.getString("imdbRating"));
-                progressDialog.dismiss();
+    public class DownLoadBody extends AsyncTask<String, Long, String> {
+        //if AddEditAct start From InternetAct Auto start AsyncTask to download the Body
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            // popup progressdialog while loading movie body
+            progressDialog = new ProgressDialog(AddEditAct.this);
+            progressDialog.setTitle("loading data111! \n please wait.....");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            //get the api of the ImdbID
+            StringBuilder response = null;
+            response = new StringBuilder();
+            try {
+                URL website = new URL(params[0]);
+                URLConnection connection = website.openConnection();
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                connection.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    response.append(inputLine);
+                in.close();
+
+            } catch (Exception ee) {
+                ee.printStackTrace();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
+            if (response == null) {
+                response.append("error");
+            }
+
+            return response.toString();
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String resultObject) {
+            //convert Api to JSON and SetText to BodyET
+            try {
+                if (resultObject == "error") {
+                    Toast.makeText(AddEditAct.this, "DID NOT FIND MOVIE,PLEASE TRY AGAIN.", Toast.LENGTH_SHORT).show();
+                } else {
+                    JSONObject mainObject = new JSONObject(resultObject);
+                    Log.d("************dsds", resultObject);
+                    BodyET.setText(mainObject.getString("Plot"));
+                    RatingET.setText(mainObject.getString("imdbRating"));
+                    progressDialog.dismiss();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
-}
 
-public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-    @Override
-    protected void onPreExecute() {
-        progressDialog = new ProgressDialog(AddEditAct.this);
-        progressDialog.setTitle("loading data222! \n please wait.....");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-        super.onPreExecute();
-    }
-
-    //when pressed the ShowBtn start the imageUrl AsyncTask
-    protected Bitmap doInBackground(String... urls) {
-        //get the image from Url
-        String urldisplay = urls[0];
-        Bitmap mIcon11 = null;
-        try {
-            InputStream in = new java.net.URL(urldisplay).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
+    public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(AddEditAct.this);
+            progressDialog.setTitle("loading data222! \n please wait.....");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+            super.onPreExecute();
         }
-        return mIcon11;
+
+        //when pressed the ShowBtn start the imageUrl AsyncTask
+        protected Bitmap doInBackground(String... urls) {
+            //get the image from Url
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        protected void onPostExecute(Bitmap result) {
+            //set the image to image view
+            bitmap = result;
+            imagebase64 = encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
+            movieIV.setImageBitmap(decodeBase64(imagebase64));
+            //linearLayout.setBackground(movieIV.getDrawable());
+            progressDialog.dismiss();
+            activity_add_edit.setFocusable(true);
+            activity_add_edit.getDrawingCache(true);
+
+        }
+
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    protected void onPostExecute(Bitmap result) {
-        //set the image to image view
-        bitmap = result;
-        imagebase64 = encodeToBase64(bitmap, Bitmap.CompressFormat.JPEG, 100);
-        movieIV.setImageBitmap(decodeBase64(imagebase64));
-        //linearLayout.setBackground(movieIV.getDrawable());
-        progressDialog.dismiss();
-        activity_add_edit.setFocusable(true);
-        activity_add_edit.getDrawingCache(true);
-
-    }
-
-}
 
     ProgressDialog progressDialog;
 
